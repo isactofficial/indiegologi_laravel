48f0203f6442528440c337c3f3087668480b62e5
ahmadrobi89 <ahmad.robi8989@gmail.com>
2025-11-06 17:23:07 +0700

    Testing Conflict

--- resources/views/testing/testing.blade.php
+++ resources/views/testing/testing.blade.php
@@ -1,1 +1,7 @@
-<h1>Testing</h1>
+<h1>Testing Confict</h1>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet molestias eveniet dolor suscipit quisquam consequatur
+rem, vero sit consequuntur unde sapiente, sequi deserunt aspernatur sint! Beatae voluptate hic temporibus eum.
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi modi quae quibusdam, repellat doloremque enim
+    asperiores. Modi voluptas eligendi adipisci, repudiandae placeat ab sunt temporibus magnam quod assumenda qui!
+    Mollitia!</p>
+<input type="text" name="" id="">
