602b4ac278b1818c145a9f497331af04d3f659c8
FadilFauzan <fadilahmadfauzan04@gmail.com>
2025-11-06 17:23:29 +0700

    conflict test

--- resources/views/testing/testing.blade.php
+++ resources/views/testing/testing.blade.php
@@ -1,1 +1,5 @@
 <h1>Indiegologi</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum nemo nihil consectetur praesentium amet distinctio voluptatum fugit totam excepturi optio ullam quas rem corrupti voluptas ex quibusdam, animi soluta harum!</p>
+<input type="text" name="" id="">
+<h2>hallo world</h2>
